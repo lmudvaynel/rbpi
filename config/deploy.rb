f90789f33d234a1ce2bd6b7cd8e2848adf1a2bef
Artem D <artdemko@gmail.com>
2014-04-15 15:05:58 +0300

    Update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,10 @@ set :stages, %w(production staging)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-set :application, "" #write app name there
+set :application, "rbpi" #write app name there
 
 set :scm, :git
-set :repository,  "" #place your repo there
+set :repository,  "git@github.com:lmudvaynel/rbpi.git" #place your repo there
 
 set :deploy_via, :remote_cache
 
