be568e3e24340446ecca34af7c2adce41c3231f7
Artem D <artdemko@gmail.com>
2014-04-16 14:50:10 +0300

    Partner link update

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Rbpi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
-    config.i18n.available_locales = [:ru, :en, :de]
+    config.i18n.available_locales = [:ru, :en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
